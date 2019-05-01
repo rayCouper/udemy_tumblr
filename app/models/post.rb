fe5e3f6d9cb785729e0b39221bcf193e603fa80e
gregoryawesoman <ray_couper@mail.ru>
2019-05-01 20:07:00 +0300

    add validation to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 25 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 255 }
 end
